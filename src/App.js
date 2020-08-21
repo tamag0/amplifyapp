68c7d1be750da0b77797f365b30836fd87f7a8c9
Tomoka Saegusa <unknown>
2020-08-21 20:48:38 +0900

    version up5

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Part4
+          Edit <code>src/App.js</code> and save to reload. Part5
         </p>
         <a
           className="App-link"
