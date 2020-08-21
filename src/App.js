8a40431e101523337c535d147a1074e45a721985
tamago <tamag0@users.noreply.github.com>
2020-08-21 20:46:25 +0900

    version up3 (#1)
    * version up3
    
    * version up4
    
    Co-authored-by: Tomoka Saegusa <>

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. Part2
+          Edit <code>src/App.js</code> and save to reload. Part4
         </p>
         <a
           className="App-link"
